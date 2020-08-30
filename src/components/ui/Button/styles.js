1c287cef41bc109334eea65c2a4f75a194136c78
Jay <jordanchu95@gmail.com>
2020-08-29 22:14:01 -0400

    colored!

--- src/components/ui/Button/styles.js
+++ src/components/ui/Button/styles.js
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 
 export const Button = motion.custom(styled.button`
   outline: none !important;
-  ${tw`py-2 px-8 rounded-full border border-pink-500 text-white`};
+  ${tw`py-2 px-8 rounded-full border border-gray-500 text-white`};
 
-  ${({ primary }) => (primary ? tw`bg-pink-500` : tw`text-pink-500`)};
+  ${({ primary }) => (primary ? tw`bg-gray-600` : tw`text-gray-500`)};
 
   ${({ block }) => block && tw`w-full`};
 `);
