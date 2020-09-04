db2fe61ce63e74fc944f723fc26b1e8dddd6055e
Jay <jordanchu95@gmail.com>
2020-09-03 23:44:45 -0400

    gallery works!!! yay

---
+++ src/pages/gallery.js
@@ -0,0 +1,49 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+
+import Layout from 'components/Layout';
+import SEO from 'components/SEO';
+import Gallery from '../../gatsby-image-gallery/src';
+
+const GalleryPage = ({ data }) => {
+  const images = data.images.edges.map(({ node }) => node.childImageSharp)
+  // Override some of Lightbox options to localise labels in French
+  const lightboxOptions = {
+    imageLoadErrorMessage: 'Impossible de charger cette image',
+    nextLabel: 'Image suivante',
+    prevLabel: 'Image précédente',
+    zoomInLabel: 'Zoomer',
+    zoomOutLabel: 'Dézoomer',
+    closeLabel: 'Fermer',
+  }
+  return (
+    <Layout>
+      <SEO title="Gallery" />
+      <Gallery images={images} lightboxOptions={lightboxOptions} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ImagesForGallery {
+    images: allFile(
+      filter: { relativeDirectory: { eq: "gallery" } }
+      sort: { fields: name }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            thumb: fluid(maxWidth: 500, maxHeight: 500) {
+              ...GatsbyImageSharpFluid
+            }
+            full: fluid(maxWidth: 1024) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default GalleryPage;
