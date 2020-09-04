5f780b1abd771cea1c45d0e33ed3b338f3957880
Jay <jordanchu95@gmail.com>
2020-09-03 22:45:45 -0400

    moved gatsby image folder

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
-import Gallery from 'src/gatsby-image-gallery';
+import Gallery from '../../gatsby-image-gallery/src';
 
 const GalleryPage: React.FC = () => {
     return (
